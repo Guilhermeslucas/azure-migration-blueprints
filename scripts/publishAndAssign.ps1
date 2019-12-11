3e9ea9ab44b984ac4e223fd00d0d698cd077fc42
Guilherme Lucas da Silva <gulucasd@microsoft.com>
2019-12-11 14:59:35 -0300

    ADD: adding parameters in order to show how to use them

--- scripts/publishAndAssign.ps1
+++ scripts/publishAndAssign.ps1
@@ -5,7 +5,7 @@ param(
 )
 
 # this is a command in powershell
-Import-AzBlueprintWithArtifact -Name $blueprintName -SubscriptionId $subscriptionId -InputPath  "./blueprint/" -Force
+Import-AzBlueprintWithArtifact -Name $blueprintName -SubscriptionId $subscriptionId -InputPath  "../blueprint/" -Force
 
 # Get the blueprint we just created
 $bp = Get-AzBlueprint -Name $blueprintName -SubscriptionId $subscriptionId
@@ -15,7 +15,10 @@ Publish-AzBlueprint -Blueprint $bp -Version $blueprintVersion
 # Get the version of the blueprint you want to assign, which we will pas to New-AzBlueprintAssignment
 $publishedBp = Get-AzBlueprint -SubscriptionId $subscriptionId -Name $blueprintName -LatestPublished
 
+#define the parameters that will be used to assign the blueprint
+$params = @{ vmSizeGen = "Standard_A2" }
+
 # Assign the new blueprint to the specified subscription (Assignment updates should use Set-AzBlueprintAssignment
 $joinedName = -join($blueprintName, $blueprintVersion)
 
-New-AzBlueprintAssignment -Blueprint $publishedBp -Location eastus -SubscriptionId $subscriptionId -Name $joinedName.Replace(".","")
+New-AzBlueprintAssignment -Blueprint $publishedBp -Location eastus -SubscriptionId $subscriptionId -Name $joinedName.Replace(".","") -Parameter $params
