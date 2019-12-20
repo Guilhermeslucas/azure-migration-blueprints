aa04d64de7aa0a26d3749896fc7503313ca7271f
Guilherme Lucas da Silva <gulucasd@microsoft.com>
2019-12-20 14:51:11 -0300

    ADD: adding the CI/CD for both subscriptions

--- scripts/publishAndAssign.ps1
+++ scripts/publishAndAssign.ps1
@@ -1,5 +1,6 @@
 param(
     [Parameter(Mandatory=$true)]$blueprintName,
+    [Parameter(Mandatory=$true)]$blueprintPath,
     [Parameter(Mandatory=$true)]$subscriptionId,
     [Parameter(Mandatory=$true)]$blueprintVersion
 )
@@ -7,7 +8,7 @@ param(
 . "./scripts/common.ps1"
 
 # this is a command in powershell
-Import-AzBlueprintWithArtifact -Name $blueprintName -SubscriptionId $subscriptionId -InputPath  "./blueprint/" -Force
+Import-AzBlueprintWithArtifact -Name $blueprintName -SubscriptionId $subscriptionId -InputPath $blueprintPath -Force
 
 # Get the blueprint we just created
 $bp = Get-AzBlueprint -Name $blueprintName -SubscriptionId $subscriptionId
@@ -17,11 +18,9 @@ Publish-AzBlueprint -Blueprint $bp -Version $blueprintVersion
 # Get the version of the blueprint you want to assign, which we will pas to New-AzBlueprintAssignment
 $publishedBp = Get-AzBlueprint -SubscriptionId $subscriptionId -Name $blueprintName -LatestPublished
 
-#define the parameters that will be used to assign the blueprint
-$params = @{ vmSizeGen = "Standard_A2" }
 
 # Assign the new blueprint to the specified subscription (Assignment updates should use Set-AzBlueprintAssignment
 $joinedName = generateName -blueprintName $blueprintName -blueprintVersion $blueprintVersion
 
 # Submit a new assignment with the deserved values
-New-AzBlueprintAssignment -Blueprint $publishedBp -Location eastus -SubscriptionId $subscriptionId -Name $joinedName -Parameter $params
+New-AzBlueprintAssignment -Blueprint $publishedBp -Location eastus -SubscriptionId $subscriptionId -Name $joinedName
